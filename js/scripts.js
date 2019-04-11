416edf547c355b1e7a8fef492e835fbd955b1869
AJ Ancheta, Megan Schulte and Reese Lee <megan@>
2019-04-11 10:20:41 -0700

    add switchActive function so it toggles between player1 and player2

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 // Business Logic for AddressBook ---------
 //creates empty contacts array and initialize current id at 0
-function PiggyDice() {
-  this.player = [],
-  this.currentId = 0
+function PiggyDice(player) {
+  this.players = [],
+  this.currentId = -1
 }
 
 PiggyDice.prototype.addPlayer = function(player) {
@@ -16,27 +16,74 @@ PiggyDice.prototype.assignId = function() {
 }
 
 // Business Logic for Players ---------
-function Player(playerName, totalScore) {
-  this.playerName = playerName;
-  this.totalScore = totalScore;
+function Player(playerName) {
+  this.playerName = playerName,
+  this.currentScore = 0,
+  this.totalScore = 0,
+  this.currentId = 0,
+  this.isActive = false
 }
+
+//very helpful!! to find which player is active.
+PiggyDice.prototype.getActiveIndex = function(){
+  for (i=0; i < this.players.length; i++){
+    if (this.players[i].isActive){
+      return i;
+    }
+  }
+}
+
+PiggyDice.prototype.switchActive = function() {
+  var activeIndex = this.getActiveIndex();
+  console.log(activeIndex);
+  if (activeIndex === 0){
+    this.players[0].isActive = false;
+    this.players[1].isActive = true;
+  } else {
+    this.players[0].isActive = true;
+    this.players[1].isActive = false;
+  }
+}
+var playerOne = new Player();
+var playerTwo = new Player();
+var game = new PiggyDice();
+game.addPlayer(playerOne);
+game.addPlayer(playerTwo);
+playerOne.isActive = true;
 
 var currentScore = 0;
 function currentRoll() {
   var result = (Math.floor(Math.random() * 6) +1);
-  // currentScore += result;
-    if (result === 1) {
-      currentScore = -1;
-
-      alert("Womp womp. You rolled a 1. Your turn is over.");
-    } else {
-      currentScore;
+    if (playerOne.isActive) {
+      if (result === 1) {
+        playerOne.currentScore = 0;
+        $("#playerOneScore").text(playerOne.currentScore);
+        alert("Womp womp. You rolled a 1. Your turn is over.");
+        game.switchActive();
+      } else {
+        playerOne.currentScore += result;
+        console.log(playerOne.currentScore);
+        $("#playerOneScore").text(playerOne.currentScore);
+        $("#die").text(result);
+      }
+      return false;
     }
-  return result;
+    if (playerTwo.isActive) {
+      if (result === 1) {
+        playerTwo.currentScore = 0;
+        $("#playerTwoScore").text(playerTwo.currentScore);
+        alert("Womp womp. You rolled a 1. Your turn is over.");
+        game.switchActive();
+      } else {
+        playerTwo.currentScore += result;
+        $("#playerTwoScore").text(playerTwo.currentScore);
+        $("#die").text(result);
+      }
+      return false;
+    }
 };
 
-var playerOne = new Player();
-var playerTwo = new Player();
+// UI Logic
 
 $(document).ready(function() {
   // attachContactListeners();
@@ -44,16 +91,16 @@ $(document).ready(function() {
     event.preventDefault();
     var player1name = $("input#playerOneName").val();
     var player2name = $("input#playerTwoName").val();
+
     $("#nameInput").hide();
     $("#playerOneInput").text(player1name);
     $("#playerTwoInput").text(player2name);
   });
   $("button#playButton").click(function(event) {
     event.preventDefault();
-    console.log("sdfs");
-    var result = currentRoll();
-    $("#die").text(result);
-    var current = currentScore += result;
-    $("#playerOneScore").text(current);
+    // console.log("sdfs");
+    currentRoll();
+    // var current = currentScore += result;
+
   });
 });
